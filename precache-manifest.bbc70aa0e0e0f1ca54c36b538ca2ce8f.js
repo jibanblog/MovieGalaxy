498402fbd95c8b8b279e16e1a50719f1833277b6
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2020-07-13 23:35:52 +0530

    Updates

---
+++ precache-manifest.bbc70aa0e0e0f1ca54c36b538ca2ce8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bb4cae77f423d753df36952ea5d452",
+    "url": "/moviegalaxy/index.html"
+  },
+  {
+    "revision": "c80fe6728338219ad76d",
+    "url": "/moviegalaxy/static/css/main.f928a283.chunk.css"
+  },
+  {
+    "revision": "740a659fe7a9f61bd8b5",
+    "url": "/moviegalaxy/static/js/2.6816ac8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviegalaxy/static/js/2.6816ac8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80fe6728338219ad76d",
+    "url": "/moviegalaxy/static/js/main.86a81666.chunk.js"
+  },
+  {
+    "revision": "91b8c60f4e2546b302d0",
+    "url": "/moviegalaxy/static/js/runtime-main.95b902e3.js"
+  }
+]);
